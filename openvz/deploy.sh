834745b7f259078ddfa64eacce599f8b1716aed7
jovistar <jovistar@gmail.com>
2015-07-03 18:29:02 +0800

    add

--- openvz/deploy.sh
+++ openvz/deploy.sh
@@ -4,15 +4,10 @@ wget -O- http://shadowsocks.org/debian/1D27208A.gpg | sudo apt-key add -
 echo "deb http://shadowsocks.org/debian wheezy main" >> /etc/apt/sources.list
 
 apt-get update
-apt-get install -y shadowsocks-libev pdnsd pptpd
-
-#pdnsd
-mv pdnsd.conf /etc/pdnsd.conf
-mv pdnsd /etc/default/pdnsd
-service pdnsd start
+apt-get install -y shadowsocks-libev
 
 #ss
 mv ss.json /root
-ss-server -c /root/ss.json
+ss-server -c /root/ss.json &
 
 exit 0
