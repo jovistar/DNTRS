d41c89885efdd1ab5a3d9f20f5415d2735632bdd
jovistar <jovistar@gmail.com>
2015-07-13 16:20:16 +0800

    add pptp

--- control/pptp_start.sh
+++ control/pptp_start.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
 echo "jovistar * lijinghua1 *" > /chap-secrets
-
 docker run -d --privileged -p 1723:1723 -v /chap-secrets:/etc/ppp/chap-secrets jovistar/pptp
--- dockerfiles/pptp/entrypoint.sh
+++ dockerfiles/pptp/entrypoint.sh
@@ -2,5 +2,6 @@
 
 echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
 sysctl -p
+
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 pptpd -f
