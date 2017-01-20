68e65c94f45500c11c16b97fb98473b5ceca86e3
jovistar <jovistar@gmail.com>
2017-01-20 23:46:05 +0800

    Bug Fixed

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -8,7 +8,7 @@ git config --global user.name "jovistar"
 git config --global push.default simple
 
 curl -s http://yum.dockerproject.org/gpg | apt-key add
-add-apt-repository "deb https://apt.dockerproject.org/repo/pool/ $(lsb_release -cs) main"
+add-apt-repository "deb https://apt.dockerproject.org/repo/ ubuntu-$(lsb_release -cs) main"
 
 apt-get update
 apt-get install -y docker-engine
