173dc260f0ee66f347e54c33af15e4d1658c06d0
jovistar <jovistar@gmail.com>
2017-10-17 14:21:26 +0800

    Installation of Docker Updated.

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,21 +1,19 @@
 #!/bin/sh
 
 apt-get update
-apt-get install -y git apt-transport-https ca-certificates curl
+apt-get install -y git apt-transport-https ca-certificates curl software-properties-common
 
 git config --global user.email "jovistar@gmail.com"
 git config --global user.name "jovistar"
 git config --global push.default simple
 
-curl -s http://yum.dockerproject.org/gpg | apt-key add
-add-apt-repository "deb https://apt.dockerproject.org/repo/ ubuntu-$(lsb_release -cs) main"
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
 apt-get update
-apt-get install -y docker-engine
+apt-get install -y docker-ce
 
-docker run hello-world
-
-#docker pull jovistar/ss
+docker pull jovistar/ss
 #docker pull jovistar/pdnsd
 #docker pull jovistar/ocserv
 #docker pull jovistar/ngrok
